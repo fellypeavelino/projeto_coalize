312cd2ec9048bdbf51738a3c24b07f0bb1dd93b2
fellypeavelino <fellypeavelino@hotmail.com>
2024-03-30 22:39:41 -0300

    fixando o ip do mysql

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     #'dsn' => 'mysql:host=127.0.0.1:48002;dbname=coalize',
-    'dsn' => 'mysql:host=172.23.0.4:3306;dbname=coalize',
+    'dsn' => 'mysql:host=172.18.0.4:3306;dbname=coalize',
     'username' => 'coalize',
     'password' => '123',
     'charset' => 'utf8',
